665e805306243a51ad7a8d1a469d30196075580e
tomekstanczak <tomek.stanczak87@gmail.com>
2023-12-02 21:18:25 +0100

    feat: add cats search

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_iSxpNY0J1wpUWrzEgbVQIM9euiUJCRkeaIGsnUnc6Ftz2d2845tTYt5pYnp02qHD';
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
+
 const catInfo = document.querySelector('.cat-info');
+const breeds = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+function fetchBreeds() {
+  return axios.get('/breeds').then(response => {
+    return response.data;
+  });
+}
+
+function fetchCatByBreed(breedId) {
+  return axios.get(`/images/search?breed_ids=${breedId}`).then(response => {
+    return response.data;
+  });
+}
+
+fetchBreeds().then(data => {
+  const html = data.map(
+    breed => `<option value="${breed.id}">${breed.name}</option>`
+  );
+  breeds.innerHTML = html;
+});
+
+breeds.addEventListener('change', ev => {
+  const breed = ev.target.value;
+  fetchCatByBreed(breed).then(cats => {
+    loader.innerHTML = cats.map(
+      cat => `<h2>Cat ID: ${cat.id}</h2><img src="${cat.url}"></img>`
+    );
+  });
+});
