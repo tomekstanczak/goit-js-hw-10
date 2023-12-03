7f47320e53168b72126cc787a66b3d1e1a475b4b
tomekstanczak <tomek.stanczak87@gmail.com>
2023-12-03 07:41:16 +0100

    feat: repair

--- src/index.js
+++ src/index.js
@@ -23,21 +23,33 @@ fetchBreeds().then(data => {
   breeds.innerHTML = html;
 });
 
-const promise = new Promise((resolve, reject) => {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    resolve();
-  } else {
-    reject();
-  }
-});
+function fetchCatByBreed(breedId) {
+  return axios.get(`/images/search?breed_ids=${breedId}`).then(response => {
+    return response.data;
+  });
+}
+
+function getChances(breed) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve(fetchCatByBreed(breed));
+    } else {
+      reject('We have a problem');
+    }
+  });
+}
 
 breeds.addEventListener('change', ev => {
   const breed = ev.target.value;
-  catFromId(breed).then(cats => {
-    loader.innerHTML = cats.map(
-      cat => `<img width="600" height="400" src="${cat.url}"></img>`
-    );
-    errorMsg.innerHTML = `<h2>${cats[0].breeds[0].name}</h2><p>${cats[0].breeds[0].description}</p>`;
-  });
+  getChances(breed)
+    .then(cats => {
+      loader.innerHTML = cats.map(
+        cat => `<img width="600" height="400" src="${cat.url}"></img>`
+      );
+      errorMsg.innerHTML = `<h2>${cats[0].breeds[0].name}</h2><p>${cats[0].breeds[0].description}</p>`;
+    })
+    .catch(error => {
+      errorMsg.insertAdjacentHTML('beforeend', '<p>Error</p>');
+    });
 });
